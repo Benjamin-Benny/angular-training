ba089aa00d491804707dae16506809667d2def0f
benjaminbenny <benjaminbennybinju@gmail.com>
2020-09-30 05:05:51 -0700

    second commit

---
+++ tour of heroes/src/app/sample-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Superman'},
+  { id: 2, name: 'Wonderwoman' },
+  { id: 3, name: 'Batman' },
+  { id: 4, name: 'Ironman' },
+  { id: 5, name: 'Cap' },
+  { id: 6, name: 'Dr.Remorey' },
+  { id: 7, name: 'Thorin Oekenshield' },
+  { id: 8, name: 'Vision' },
+  { id: 9, name: 'Gandalf' },
+  { id: 10, name: 'Magneto' },
+  { id: 11, name: 'Scarlett' }
+];
